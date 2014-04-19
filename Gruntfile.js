d02ea9d903fe379686077c2228c954ea6ca87cd2
Doug March <dougmarch@gmail.com>
2014-04-19 13:52:59 -0400

    renamed some folders and more cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,95 +1,157 @@
+/*!
+ * Grunt
+ * $ npm install grunt-contrib-uglify grunt-autoprefixer grunt-contrib-cssmin grunt-contrib-imagemin grunt-contrib-sass grunt-contrib-watch grunt-contrib-concat grunt-contrib-clean grunt-contrib-jshint grunt-notify --save-dev
+ */
+
 module.exports = function(grunt) {
+
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-       banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      build: {
-       src: 'js/dist/scripts.js',
-       dest: 'js/dist/scripts.min.js'
-      }
-    },
-    concat: {
-      dist: {
-        src: ['js/src/vendor/*.js','js/src/*.js'],
-        dest: 'js/dist/scripts.js'
-      }
-    },
+
+    // Sass
     sass: {
       dist: {
         options: {
-        style: 'expanded'
+          style: 'expanded'
         },
         files: {
-        'css/style.css' : 'scss/style.scss'
+          'assets/styles/main.css': 'styles/main.scss'
         }
       }
     },
+
+    // Autoprefix
     autoprefixer: {
-      single_file: {
-        src: 'css/style.css',
-        dest:'css/style.css'
-      }
-    },
-    assemble: {
       options: {
-        assets: 'assets',
-        plugins: ['permalinks'],
-        partials: ['includes/**/*.hbs'],
-        layout: ['layouts/default.hbs']
+        browsers: [
+          'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'
+        ]
       },
-      pages: {
-        src: ['docs/*.hbs'],
-        dest: './'
+      dist: {
+        src: 'assets/styles/main.css'
       }
     },
-    connect: {
-      server: {
-      options: {}
-      }
-    },
-    watch: {
-      js: {
-        files: ['js/src/*.js'],
-        tasks: ['concat','uglify'],
-      options: {
-        spawn: false
-      }
-    },
-    scss: {
-      files: ['scss/*.scss'],
-      tasks: ['sass','autoprefixer']
-    },
-    html: {
-      files: ['*.html'],
-      options: {
-        livereload: true
-      }
-    },
-    css: {
-      files: ['css/*.css'],
-      options: {
-          livereload: true
+
+    // CSS minify
+    cssmin: {
+      dist: {
+        files: {
+          'assets/styles/main.min.css': 'assets/styles/main.css'
         }
       }
     },
-    cssmin: {
-      minify: {
-        src:['css/*.css','!css/*.min.css'],
-        dest: 'css/style.min.css'
+
+    // JShint
+    jshint: {
+      files: ['js/**/*.js'],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    // Concat
+    concat: {
+      js: {
+        src: ['js/**/*.js'],
+        dest: 'assets/js/main.js'
+      },
+    },
+
+    // Uglify
+    uglify: {
+      dist: {
+        src: 'assets/scripts/main.js',
+        dest: 'assets/scripts/main.min.js'
+      },
+    },
+
+    // Imagemin
+    imagemin: {
+      dist: {
+        options: {
+          optimizationLevel: 3,
+          progressive: true,
+          interlaced: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'images',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'assets/images'
+        }]
+      }
+    },
+
+    // Clean
+    clean: {
+      build: ['assets/styles', 'assets/scripts', 'assets/images']
+    },
+
+    // Notify
+    notify: {
+      styles: {
+        options: {
+          message: 'Styles task complete',
+        }
+      },
+      scripts: {
+        options: {
+          message: 'Scripts task complete',
+        }
+      },
+      images: {
+        options: {
+          message: 'Images task complete',
+        }
+      },
+    },
+
+    // Watch
+    watch: {
+      styles: {
+        files: 'styles/**/*.scss',
+        tasks: ['sass', 'autoprefixer', 'cssmin', 'notify:styles'],
+      },
+      scripts: {
+        files: 'js/**/*.js',
+        tasks: ['concat', 'uglify', 'notify:scripts'],
+      },
+      images: {
+        files: 'images/**/*',
+        tasks: ['imagemin', 'notify:images'],
+      },
+      livereload: {
+        options: { livereload: true },
+        files: [
+          'assets/styles/**/*.css',
+          'assets/scripts/**/*.js',
+          'assets/images/**/*'
+        ]
       }
     }
-
   });
+
+  // Default task
+  grunt.registerTask('default', [
+    'jshint',
+    'clean',
+    'concat',
+    'uglify',
+    'sass',
+    'autoprefixer',
+    'cssmin',
+    'imagemin'
+  ]);
+
+  // Load plugins
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.registerTask('default',['concat','uglify']);
-  grunt.registerTask('serve', [ 'connect','watch' ]);
-  grunt.registerTask('build', [ 'sass','cssmin','concat','uglify']);
-}
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-notify');
+
+};
