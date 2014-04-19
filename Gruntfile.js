a87f0adf4391f21821b40e941858bc22a6cb25d8
Doug March <dougmarch@gmail.com>
2014-04-18 20:39:38 -0400

    adding more variants to the index page and cleaning up duplicate files in the project

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,76 +8,88 @@ module.exports = function(grunt) {
       build: {
        src: 'js/dist/scripts.js',
        dest: 'js/dist/scripts.min.js'
-     }
-   },
-   concat: {
-    dist: {
-      src: ['js/src/vendor/*.js','js/src/*.js'],
-      dest: 'js/dist/scripts.js'
-    }
-  },
-  sass: {
-    dist: {
+      }
+    },
+    concat: {
+      dist: {
+        src: ['js/src/vendor/*.js','js/src/*.js'],
+        dest: 'js/dist/scripts.js'
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+        style: 'expanded'
+        },
+        files: {
+        'css/style.css' : 'scss/style.scss'
+        }
+      }
+    },
+    autoprefixer: {
+      single_file: {
+        src: 'css/style.css',
+        dest:'css/style.css'
+      }
+    },
+    assemble: {
       options: {
-       style: 'expanded'
-     },
-     files: {
-       'css/style.css' : 'scss/style.scss'
-     }
-   }
- },
- autoprefixer: {
-  single_file: {
-    src: 'css/style.css',
-    dest:'css/style.css'
-  }
-},
-connect: {
-  server: {
-   options: {}
- }
-},
-watch: {
-  js: {
-    files: ['js/src/*.js'],
-    tasks: ['concat','uglify'],
-    options: {
-     spawn: false
-   }
- },
- scss: {
-  files: ['scss/*.scss'],
-  tasks: ['sass','autoprefixer']
-},
-html: {
-  files: ['*.html'],
-  options: {
-   livereload: true
- }
-},
-css: {
-  files: ['css/*.css'],
-  options: {
-   livereload: true
- }
-}
-},
-cssmin: {
-  minify: {
-    src:['css/*.css','!css/*.min.css'],
-    dest: 'css/style.min.css'
-  }
-}
+        assets: 'assets',
+        plugins: ['permalinks'],
+        partials: ['includes/**/*.hbs'],
+        layout: ['layouts/default.hbs']
+      },
+      pages: {
+        src: ['docs/*.hbs'],
+        dest: './'
+      }
+    },
+    connect: {
+      server: {
+      options: {}
+      }
+    },
+    watch: {
+      js: {
+        files: ['js/src/*.js'],
+        tasks: ['concat','uglify'],
+      options: {
+        spawn: false
+      }
+    },
+    scss: {
+      files: ['scss/*.scss'],
+      tasks: ['sass','autoprefixer']
+    },
+    html: {
+      files: ['*.html'],
+      options: {
+        livereload: true
+      }
+    },
+    css: {
+      files: ['css/*.css'],
+      options: {
+          livereload: true
+        }
+      }
+    },
+    cssmin: {
+      minify: {
+        src:['css/*.css','!css/*.min.css'],
+        dest: 'css/style.min.css'
+      }
+    }
 
-});
-grunt.loadNpmTasks('grunt-contrib-uglify');
-grunt.loadNpmTasks('grunt-contrib-concat');
-grunt.loadNpmTasks('grunt-contrib-sass');
-grunt.loadNpmTasks('grunt-contrib-watch');
-grunt.loadNpmTasks('grunt-contrib-connect');
-grunt.loadNpmTasks('grunt-contrib-cssmin');
-grunt.loadNpmTasks('grunt-autoprefixer');
-grunt.registerTask('default',['concat','uglify']);
-grunt.registerTask('serve', [ 'connect','watch' ]);
-grunt.registerTask('build', [ 'sass','cssmin','concat','uglify']);
+  });
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.registerTask('default',['concat','uglify']);
+  grunt.registerTask('serve', [ 'connect','watch' ]);
+  grunt.registerTask('build', [ 'sass','cssmin','concat','uglify']);
 }
